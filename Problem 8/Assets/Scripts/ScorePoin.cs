ad8bfbaff175ad388e6ad78a5a343df704a117de
Adi Wibowo <adiwibowo0317@gmail.com>
2021-10-08 23:30:43 +0700

    Problem-8

---
+++ Problem 8/Assets/Scripts/ScorePoin.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScorePoin : MonoBehaviour
+{
+    public float score;
+    public Text scoreUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("box"))
+        {
+            score += 1;
+            scoreUI.text = score.ToString();
+            Destroy(collision.gameObject);
+        }
+    }
+}
