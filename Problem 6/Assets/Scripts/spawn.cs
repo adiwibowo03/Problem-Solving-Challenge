1edfdf178e50ac72211e22155aac29d11dd31cfd
Adi Wibowo <adiwibowo0317@gmail.com>
2021-10-08 21:54:58 +0700

    Problem-6

---
+++ Problem 6/Assets/Scripts/spawn.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn : MonoBehaviour
+{
+    public GameObject obj;
+    public float radius;
+    public float randomPositionX;
+    public float randomPositionY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObject", 0f, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObject(){
+        randomPositionX = Random.Range(-8f, 8f);
+        randomPositionY = Random.Range(-4f, 4f);
+        transform.position = new Vector2(randomPositionX, randomPositionY);
+        Instantiate(obj, transform.position, Quaternion.identity);
+    }
+}
